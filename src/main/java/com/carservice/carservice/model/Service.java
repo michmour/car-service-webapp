a820a3e9aab6703d9a630dde3fbbbca45053e3a5
George Koukoulakis <riblee@gmail.com>
2017-10-12 19:45:27 +0300

    entities add(not completed, jpa works)

--- src/main/java/com/carservice/carservice/model/Service.java
+++ src/main/java/com/carservice/carservice/model/Service.java
@@ -1,4 +1,59 @@
 package com.carservice.carservice.model;
 
-public class Service {
+
+import javax.persistence.*;
+import javax.validation.constraints.Digits;
+import java.io.Serializable;
+import java.sql.Timestamp;
+import java.util.Set;
+
+@Entity
+@Table(name="service")
+public class Service implements Serializable {
+
+    @Id
+    @Column(name = "serviceid", nullable = false)
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long serviceid;
+
+    @Column(name = "servicedate")
+    private java.sql.Timestamp servicedate;
+
+
+    @Column(name = "status")
+    private int status;
+
+    @Column(name = "type")
+    private int type;
+
+    @Digits(integer=10, fraction=2)
+    @Column(name = "servicecost")
+    private String servicecost;
+
+    @Column(name = "details")
+    private String details;
+
+    @ManyToOne(optional=false)
+    @JoinColumn(name="userid",referencedColumnName= "userid")
+    private User userid;
+
+    public Service() {
+
+    }
+
+
+//        @OneToMany(mappedBy="userid",targetEntity= Vehicle.class)
+//        private Set<Vehicle> vehiclescollection;
+
+
+
+    public Service(long serviceid, Timestamp servicedate, int status, int type, String servicecost, String details, User userid) {
+        this.serviceid = serviceid;
+        this.servicedate = servicedate;
+        this.status = status;
+        this.type = type;
+        this.servicecost = servicecost;
+        this.details = details;
+        this.userid = userid;
+    }
 }
--- src/main/java/com/carservice/carservice/model/User.java
+++ src/main/java/com/carservice/carservice/model/User.java
@@ -1,4 +1,126 @@
 package com.carservice.carservice.model;
 
-public class User {
-}
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Set;
+
+@Entity
+    @Table(name="user")
+    public class User implements Serializable {
+
+        @Id
+        @Column(name = "userid", nullable = false)
+        @GeneratedValue(strategy = GenerationType.AUTO)
+        private long userid;
+
+        @Column(name = "ssn")
+        private int ssn;
+
+        @Column(name = "name")
+        private String name;
+
+        @Column(name = "surname")
+        private String surname;
+
+        @Column(name = "address")
+        private String address;
+
+        @Column(name = "email")
+        private String email;
+
+        @Column(name = "password")
+        private String password;
+
+        @Column(name = "usertype")
+        private int usertype;
+
+
+        public User() {
+
+        }
+
+        public User(long userid, int ssn, String name, String surname, String address, String email, String password, int usertype, User order) {
+            this.userid = userid;
+            this.ssn = ssn;
+            this.name = name;
+            this.surname = surname;
+            this.address = address;
+            this.email = email;
+            this.password = password;
+            this.usertype = usertype;
+        }
+
+
+//        @OneToMany(mappedBy="userid",targetEntity= Vehicle.class)
+//        private Set<Vehicle> vehiclescollection;
+
+        @OneToMany(mappedBy="userid",targetEntity= Service.class)
+        private Set<Service>servicescollection;
+
+
+    public long getUserid() {
+            return userid;
+        }
+
+        public void setUserid(long userid) {
+            this.userid = userid;
+        }
+
+        public int getSsn() {
+            return ssn;
+        }
+
+        public void setSsn(int ssn) {
+            this.ssn = ssn;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getSurname() {
+            return surname;
+        }
+
+        public void setSurname(String surname) {
+            this.surname = surname;
+        }
+
+        public String getAddress() {
+            return address;
+        }
+
+        public void setAddress(String address) {
+            this.address = address;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public void setEmail(String email) {
+            this.email = email;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+
+        public void setPassword(String password) {
+            this.password = password;
+        }
+
+        public int getUsertype() {
+            return usertype;
+        }
+
+        public void setUsertype(int usertype) {
+            this.usertype = usertype;
+        }
+
+    }
